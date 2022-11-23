7f8629535d392e6babdcc78c94d765c92cbaa7f4
Wellington <17155519+WellingtonWritesCode@users.noreply.github.com>
2022-11-23 16:19:31 -0300

    Add files via upload

--- pesquisa/AVL.java
+++ pesquisa/AVL.java
@@ -5,47 +5,43 @@ import java.util.LinkedList;
 import Av1C2.Conta;
 
 public class AVL extends Metodo {
-	
+
 	private NoAVL raiz;
-	private boolean h;
-	
-	public AVL() {
-		raiz = null;
-		h = true;
-	}
-	
+	private boolean h = true;
+
 	@Override
 	public void insere(Conta conta) {
 		raiz = insere(conta, raiz);
 	}
 
 	private NoAVL insere(Conta conta, NoAVL no) {
-		if(no == null) {
+		if (no == null) {
 			h = true;
 			return new NoAVL(conta);
-		} else if(no.getNome().equals(conta.getNome())) {
+		} else if (no.getNome().equals(conta.getNome())) {
 			no.setInfo(conta);
 			return no;
 		}
-		if(no.getNome().compareTo(conta.getNome()) > 0) {
+		if (no.getNome().compareTo(conta.getNome()) > 0) {
 			no.setEsq(insere(conta, no.getEsq()));
 			no = balancearDir(no);
-		} else if(no.getNome().compareTo(conta.getNome()) < 0) {
+			return no;
+		} else {
 			no.setDir(insere(conta, no.getDir()));
 			no = balancearEsq(no);
+			return no;
 		}
-		return no;
 	}
 
 	private NoAVL balancearDir(NoAVL no) {
-		if(h) {
-			switch(no.getFatorBalanceamento()) {
+		if (h) {
+			switch (no.getFatorBalanceamento()) {
 			case 1:
-				no.setFatorBalanceamento((byte)0);
+				no.setFatorBalanceamento((byte) 0);
 				h = false;
 				break;
 			case 0:
-				no.setFatorBalanceamento((byte)-1);
+				no.setFatorBalanceamento((byte) -1);
 				break;
 			case -1:
 				no = rotacaoDireita(no);
@@ -55,14 +51,14 @@ public class AVL extends Metodo {
 	}
 
 	private NoAVL balancearEsq(NoAVL no) {
-		if(h) {
-			switch(no.getFatorBalanceamento()) {
+		if (h) {
+			switch (no.getFatorBalanceamento()) {
 			case -1:
-				no.setFatorBalanceamento((byte)0);
+				no.setFatorBalanceamento((byte) 0);
 				h = false;
 				break;
 			case 0:
-				no.setFatorBalanceamento((byte)1);
+				no.setFatorBalanceamento((byte) 1);
 				break;
 			case 1:
 				no = rotacaoEsquerda(no);
@@ -74,9 +70,10 @@ public class AVL extends Metodo {
 	private NoAVL rotacaoDireita(NoAVL no) {
 		NoAVL temp1, temp2;
 		temp1 = no.getEsq();
-		if(temp1.getFatorBalanceamento() == -1) {
+		if (temp1.getFatorBalanceamento() == -1) {
+			no.setEsq(temp1.getDir());
 			temp1.setDir(no);
-			no.setFatorBalanceamento((byte)0);
+			no.setFatorBalanceamento((byte) 0);
 			no = temp1;
 		} else {
 			temp2 = temp1.getDir();
@@ -84,30 +81,28 @@ public class AVL extends Metodo {
 			temp2.setEsq(temp1);
 			no.setEsq(temp2.getDir());
 			temp2.setDir(no);
-			if(temp2.getFatorBalanceamento() == -1) {
-				no.setFatorBalanceamento((byte)0);
-			} else {
-				no.setFatorBalanceamento((byte)0);
-			}
-			if(temp2.getFatorBalanceamento() == 1) {
-				temp1.setFatorBalanceamento((byte)-1);
-			} else {
-				temp1.setFatorBalanceamento((byte)0);
-			}
+			if (temp2.getFatorBalanceamento() == -1)
+				no.setFatorBalanceamento((byte) 1);
+			else
+				no.setFatorBalanceamento((byte) 0);
+			if (temp2.getFatorBalanceamento() == 1)
+				temp1.setFatorBalanceamento((byte) -1);
+			else
+				temp1.setFatorBalanceamento((byte) 0);
 			no = temp2;
 		}
-		no.setFatorBalanceamento((byte)0);
-		h = false;
+		no.setFatorBalanceamento((byte) 0);
+		this.h = false;
 		return no;
 	}
-	
+
 	private NoAVL rotacaoEsquerda(NoAVL no) {
 		NoAVL temp1, temp2;
 		temp1 = no.getDir();
-		if(temp1.getFatorBalanceamento() == 1) {
+		if (temp1.getFatorBalanceamento() == 1) {
 			no.setDir(temp1.getEsq());
 			temp1.setEsq(no);
-			no.setFatorBalanceamento((byte)0);
+			no.setFatorBalanceamento((byte) 0);
 			no = temp1;
 		} else {
 			temp2 = temp1.getEsq();
@@ -115,40 +110,38 @@ public class AVL extends Metodo {
 			temp2.setDir(temp1);
 			no.setDir(temp2.getEsq());
 			temp2.setEsq(no);
-			if(temp2.getFatorBalanceamento() == 1) {
-				no.setFatorBalanceamento((byte)-1);
-			} else {
-				no.setFatorBalanceamento((byte)0);
-			}
-			if(temp2.getFatorBalanceamento() == -1) {
-				temp1.setFatorBalanceamento((byte)1);
-			} else {
-				temp1.setFatorBalanceamento((byte)0);
-			}
+			if (temp2.getFatorBalanceamento() == 1)
+				no.setFatorBalanceamento((byte) -1);
+			else
+				no.setFatorBalanceamento((byte) 0);
+			if (temp2.getFatorBalanceamento() == -1)
+				temp1.setFatorBalanceamento((byte) 1);
+			else
+				temp1.setFatorBalanceamento((byte) 0);
 			no = temp2;
 		}
-		no.setFatorBalanceamento((byte)0);
+		no.setFatorBalanceamento((byte) 0);
 		h = false;
 		return no;
 	}
 
 	@Override
-	public LinkedList<Conta> pesquisa(String nome){
+	public LinkedList<Conta> pesquisa(String nome) {
 		NoAVL no = pesquisa(nome, raiz);
-		if(no == null) {
+		if (no == null) {
 			return null;
 		}
 		return no.getInfo();
 	}
-	
+
 	private NoAVL pesquisa(String nome, NoAVL no) {
-		if(no == null) {
+		if (no == null) {
 			return null;
 		}
-		if(no.getNome().compareTo(nome) < 0) {
+		if (no.getNome().compareTo(nome) < 0) {
 			return pesquisa(nome, no.getEsq());
 		}
-		if(no.getNome().compareTo(nome) > 0) {
+		if (no.getNome().compareTo(nome) > 0) {
 			return pesquisa(nome, no.getDir());
 		}
 		return no;
