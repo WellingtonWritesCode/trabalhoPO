de8e73a71f2057b17625dc83e885e60e524ba356
Wellington <17155519+WellingtonWritesCode@users.noreply.github.com>
2022-12-05 18:48:06 -0300

    Add files via upload

--- pesquisa/Hashing.java
+++ pesquisa/Hashing.java
@@ -6,11 +6,11 @@ import Av1C2.Conta;
 
 public class Hashing extends Metodo {
 	private LinkedList<Conta>[] lista;
-	
+
 	@SuppressWarnings("unchecked")
 	public Hashing(int tamanho) {
 		int aux;
-		switch(tamanho) {
+		switch (tamanho) {
 		case 1000:
 			aux = 1103;
 			break;
@@ -28,47 +28,54 @@ public class Hashing extends Metodo {
 		}
 		lista = new LinkedList[aux];
 	}
-	
+
 	public Hashing() {
 		this(500);
 	}
-	
+
 	@Override
 	public void insere(Conta conta) {
 		int index = getIndex(conta.getNome());
-		if(lista[index] == null) {
-			lista[index] = new LinkedList<Conta>();
+		boolean inserido = false;
+
+		while (!inserido) {
+			if (lista[index] == null) {
+				lista[index] = new LinkedList<Conta>();
+				inserido = true;
+			} else {
+				inserido = lista[index].getFirst().getNome().equals(conta.getNome());
+			}
+			if (!inserido) {
+				index = (index + 1) % lista.length;
+			}
 		}
-                boolean inserido;
-                do{
-                    inserido = lista[index].getFirst().getNome().equals(conta.getNome());
-                if(inserido){
-                    lista[index].add(conta);
-                } else {
-                    index = (index+1)%lista.length;
-                }}while(!inserido);
-		
+
+		lista[index].add(conta);
+
 	}
-	
+
 	@Override
 	public LinkedList<Conta> pesquisa(String nome) {
 		int indexInicial = getIndex(nome);
-                int index = indexInicial;
-                boolean inserido;
-                do{
-                    inserido = lista[index].getFirst().getNome().equals(nome);
-                if(inserido){
-                    return lista[index];
-                } else {
-                    index = (index+1)%lista.length;
-                    if(index == indexInicial){
-                        return null;
-                    }
-                }}while(!inserido);
-                return null;
+		int index = indexInicial;
+		boolean encontrado;
+		while (true) {
+			if(lista[index] == null) {
+				return null;
+			}
+			encontrado = lista[index].getFirst().getNome().equals(nome);
+			if (encontrado) {
+				return lista[index];
+			} else {
+				index = (index + 1) % lista.length;
+				if (index == indexInicial) {
+					return null;
+				}
+			}
+		}
 	}
-	
+
 	private int getIndex(String str) {
-		return Math.abs(str.hashCode()%lista.length);
+		return Math.abs(str.hashCode() % lista.length);
 	}
 }
